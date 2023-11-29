54bb9b3e49a98c8df1c7f53314d8d3e19c115b38
iagojsilva <iago.silva@unvoid.com>
2023-11-28 22:42:09 -0300

    Implement CRUD

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,26 +1,33 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class ProductsService {
+  constructor(private readonly _prismaService: PrismaService){}
   create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+    return this._prismaService.product.create({
+      data: {
+        ...createProductDto,
+        quantity: 0
+      }
+    })
   }
 
   findAll() {
-    return `This action returns all products`;
+    return this._prismaService.product.findMany({});
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} product`;
+    return this._prismaService.product.findFirst({where: {id}});
   }
 
   update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+    return this._prismaService.product.update({data: {...updateProductDto}, where: {id}});
   }
 
   remove(id: number) {
-    return `This action removes a #${id} product`;
+    return this._prismaService.product.delete({ where: {id}});
   }
 }
